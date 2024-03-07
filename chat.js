874382aa31ac88bacb323c6a073e6bbd4d0621e1
MADOKA SUZUKI <158795547+MadokaSuzukim@users.noreply.github.com>
2024-03-07 21:07:36 +0900

    main

--- chat.js
+++ chat.js
@@ -1,66 +1,6 @@
 // アファメーション一覧の初期化
 let affirmations = [];
 
-// チャットメッセージの送信イベント
-$("#send").on("click", function () {
-    const message = $("#text").val();
-    
-    // メッセージ内に特定のキーワードが含まれているかチェック
-    if (message.includes("アファメーション")) { // ここでは "アファメーション" というキーワードを検索する例としています
-        // アファメーション一覧に追加
-        affirmations.push(message);
-        
-        // アファメーション一覧を更新
-        updateAffirmations();
-    }
-    
-    // その他の処理...
-});
-
-// アファメーション一覧を更新する関数
-function updateAffirmations() {
-    // アファメーション一覧を表示する要素の取得
-    const $affirmationList = $("#affirmationList");
-    
-    // 一覧をクリアしてから再描画
-    $affirmationList.empty();
-    
-    // アファメーション一覧をループして表示
-    affirmations.forEach((affirmation, index) => {
-        $affirmationList.append(`<div>${index + 1}. ${affirmation}</div>`);
-    });
-}
-
-// チャットメッセージの送信イベント
-$("#send").on("click", function () {
-    const message = $("#text").val();
-    
-    // メッセージ内に特定のキーワードが含まれているかチェック
-    if (message.includes("アファメーション")) { // ここでは "アファメーション" というキーワードを検索する例としています
-        // アファメーション一覧に追加
-        affirmations.push(message);
-        
-        // アファメーション一覧を更新
-        updateAffirmations();
-    }
-    
-    // その他の処理...
-});
-
-// アファメーション一覧を更新する関数
-function updateAffirmations() {
-    // アファメーション一覧を表示する要素の取得
-    const $affirmationList = $("#affirmationList");
-    
-    // 一覧をクリアしてから再描画
-    $affirmationList.empty();
-    
-    // アファメーション一覧をループして表示
-    affirmations.forEach((affirmation, index) => {
-        $affirmationList.append(`<div>${index + 1}. ${affirmation}</div>`);
-    });
-}
-
 $(document).ready(function() {
     // ローカルストレージからアファメーションを取得して表示する
     displayAffirmations();
@@ -70,7 +10,53 @@ $(document).ready(function() {
         var newAffirmation = $("#newAffirmation").val();
         addAffirmation(newAffirmation);
     });
+
+    // チャットメッセージの送信イベント
+    $("#send").on("click", function () {
+        const message = $("#text").val();
+        
+        // メッセージ内に特定のキーワードが含まれているかチェック
+        if (message.includes("アファメーション")) {
+            // アファメーション一覧に追加
+            affirmations.push(message);
+            
+            // ローカルストレージにアファメーションを追加
+            addAffirmationToLocalStorage(message);
+            
+            // アファメーション一覧を更新
+            updateAffirmations();
+        }
+        
+        // その他の処理...
+    });
 });
+
+// ローカルストレージにアファメーションを追加する関数
+function addAffirmationToLocalStorage(affirmation) {
+    // 保存されているアファメーションを取得し、新しいアファメーションを追加
+    var affirmationsFromStorage = JSON.parse(localStorage.getItem("affirmations")) || [];
+    affirmationsFromStorage.push(affirmation);
+
+    // 更新されたアファメーションをローカルストレージに保存
+    localStorage.setItem("affirmations", JSON.stringify(affirmationsFromStorage));
+}
+
+// ローカルストレージからアファメーションを取得して表示する関数
+function displayAffirmations() {
+    // 保存されているアファメーションを取得
+    var affirmationsFromStorage = JSON.parse(localStorage.getItem("affirmations")) || [];
+
+    // アファメーション一覧を表示する要素の取得
+    const $affirmationList = $("#affirmationList");
+    
+    // 一覧をクリアしてから再描画
+    $affirmationList.empty();
+    
+    // アファメーション一覧をループして表示
+    affirmationsFromStorage.forEach((affirmation, index) => {
+        $affirmationList.append(`<div>${index + 1}. ${affirmation}</div>`);
+    });
+}
 
 // アファメーションをローカルストレージに追加する関数
 function addAffirmation(affirmation) {
@@ -85,12 +71,14 @@ function addAffirmation(affirmation) {
     displayAffirmations();
 }
 
-// ローカルストレージからアファメーションを取得して表示する関数
-function displayAffirmations() {
-    var affirmations = JSON.parse(localStorage.getItem("affirmations")) || [];
-    var $affirmationList = $("#affirmationList");
+// アファメーション一覧を更新する関数
+function updateAffirmations() {
+    // アファメーション一覧を表示する要素の取得
+    const $affirmationList = $("#affirmationList");
+    
+    // 一覧をクリアしてから再描画
     $affirmationList.empty();
-    affirmations.forEach(function(affirmation, index) {
-        $affirmationList.append("<div>" + (index + 1) + ". " + affirmation + "</div>");
-    });
+    
+    // アファメーション一覧をローカルストレージから取得して表示
+    displayAffirmations();
 }
