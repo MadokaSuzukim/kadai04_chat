db15e01ebb9ddbe167b7154cfdf203aafe3226c0
MADOKA SUZUKI <158795547+MadokaSuzukim@users.noreply.github.com>
2024-03-07 21:32:07 +0900

    main

--- chat.js
+++ chat.js
@@ -11,34 +11,52 @@ $(document).ready(function() {
         addAffirmation(newAffirmation);
     });
 
+    // アファメーション一覧の削除ボタンがクリックされたときの処理
+    $(document).on("click", ".remove", function() {
+        var index = $(this).data("index");
+        removeAffirmation(index);
+    });
+
     // チャットメッセージの送信イベント
-    $("#send").on("click", function () {
-        const message = $("#text").val();
+$("#send").on("click", function () {
+    const message = $("#text").val();
+    
+    // メッセージ内に特定のキーワードが含まれているかチェック
+    if (message.includes("アファメーション")) {
+        // ローカルストレージから現在のアファメーションの一覧を取得
+        let affirmationsFromStorage = JSON.parse(localStorage.getItem("affirmations")) || [];
         
-        // メッセージ内に特定のキーワードが含まれているかチェック
-        if (message.includes("アファメーション")) {
-            // アファメーション一覧に追加
-            affirmations.push(message);
-            
-            // ローカルストレージにアファメーションを追加
-            addAffirmationToLocalStorage(message);
-            
-            // アファメーション一覧を更新
-            updateAffirmations();
-        }
+        // 新しいアファメーションを追加
+        affirmationsFromStorage.push(message);
         
-        // その他の処理...
+        // ローカルストレージにアファメーションを保存
+        localStorage.setItem("affirmations", JSON.stringify(affirmationsFromStorage));
+        
+        // アファメーション一覧を更新
+        updateAffirmations();
+    }
     });
 });
 
 // ローカルストレージにアファメーションを追加する関数
-function addAffirmationToLocalStorage(affirmation) {
-    // 保存されているアファメーションを取得し、新しいアファメーションを追加
-    var affirmationsFromStorage = JSON.parse(localStorage.getItem("affirmations")) || [];
-    affirmationsFromStorage.push(affirmation);
+function addAffirmationToLocalStorage(affirmations) {
+    // 更新されたアファメーションをローカルストレージに保存
+    localStorage.setItem("affirmations", JSON.stringify(affirmations));
+}
+
+// アファメーション一覧からアファメーションを削除する関数
+function removeAffirmation(index) {
+    // 保存されているアファメーションを取得
+    var affirmations = JSON.parse(localStorage.getItem("affirmations")) || [];
+
+    // 指定されたインデックスのアファメーションを削除
+    affirmations.splice(index, 1);
 
     // 更新されたアファメーションをローカルストレージに保存
-    localStorage.setItem("affirmations", JSON.stringify(affirmationsFromStorage));
+    localStorage.setItem("affirmations", JSON.stringify(affirmations));
+
+    // アファメーション一覧を再描画
+    displayAffirmations();
 }
 
 // ローカルストレージからアファメーションを取得して表示する関数
@@ -54,21 +72,21 @@ function displayAffirmations() {
     
     // アファメーション一覧をループして表示
     affirmationsFromStorage.forEach((affirmation, index) => {
-        $affirmationList.append(`<div>${index + 1}. ${affirmation}</div>`);
+        // 削除ボタンを付けて表示
+        $affirmationList.append(`<div>${index + 1}. ${affirmation} <span class="remove" data-index="${index}">削除</span></div>`);
     });
 }
 
 // アファメーションをローカルストレージに追加する関数
 function addAffirmation(affirmation) {
-    // 保存されているアファメーションを取得し、新しいアファメーションを追加
-    var affirmations = JSON.parse(localStorage.getItem("affirmations")) || [];
-    affirmations.push(affirmation);
+    // 保存されているアファメーションを取得
+    var affirmationsFromStorage = JSON.parse(localStorage.getItem("affirmations")) || [];
+    
+    // 新しいアファメーションを追加
+    affirmationsFromStorage.push(affirmation);
 
-    // 更新されたアファメーションをローカルストレージに保存
-    localStorage.setItem("affirmations", JSON.stringify(affirmations));
-
-    // アファメーション一覧を更新して表示
-    displayAffirmations();
+    // ローカルストレージに更新されたアファメーションを保存
+    addAffirmationToLocalStorage(affirmationsFromStorage);
 }
 
 // アファメーション一覧を更新する関数
@@ -79,6 +97,6 @@ function updateAffirmations() {
     // 一覧をクリアしてから再描画
     $affirmationList.empty();
     
-    // アファメーション一覧をローカルストレージから取得して表示
+    // アファメーション一覧を再度表示
     displayAffirmations();
 }
